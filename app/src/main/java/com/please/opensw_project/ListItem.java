8614c30405df3f317faa096590b66b9fe256baf3
minJazz <alswo9588@gmail.com>
2021-06-01 21:33:05 +0900

    initial commit

---
+++ app/src/main/java/com/please/opensw_project/ListItem.java
@@ -0,0 +1,18 @@
+package com.please.opensw_project;
+
+public class ListItem {
+
+
+        private String btnText;
+
+        public ListItem(String text){
+            btnText = text;
+        }
+
+        public String getText(){
+            return btnText;
+        }
+
+
+
+}
---
+++ app/src/main/java/com/please/opensw_project/MainActivity.java
@@ -0,0 +1,25 @@
+package com.please.opensw_project;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ListView list = findViewById(R.id.dybnamic);
+//        list.setVisibility(View.VISIBLE);
+
+    }
+}
